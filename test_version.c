73b988b7e2ffe45b18811afbc22bed7fa1a68565
wert-rar <72702040+wert-rar@users.noreply.github.com>
2021-10-25 21:09:02 +0300

    upgrade typing, add correct angle func

--- test_version.c
+++ test_version.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <math.h>
-
-float get_angle(int off_x,int off_y,float distance)
+float  pi = 3.14;
+float get_angle(float off_x,float off_y,float distance)
 {
-	// in this case distance will be hypotenuse so we can find angle using this
-	printf("angle sin  %f \n",(asin(off_x/distance)/3.14)*180);
-	printf("angle cos  %f \n",(asin(off_y/distance)/3.14)*180);
-	return (asin(off_x/distance)/3.14)*180;
+	// in this case distance will be hypotenuse, so we can find angle using th
+	if (off_x > 0 && off_y > 0){
+	    return acosf(off_x / distance) / pi * 180;
+	}
+	else if (off_x > 0 && off_y < 0){
+	    return -acosf(off_x / distance) / pi * 180;
+	}
+	else if ( off_x < 0 && off_y > 0){
+	    return 180 - acosf(off_y / distance) / pi * 180;
+	}
+	else if (off_x < 0 && off_y < 0){
+	    return -(90 + acosf(off_y / distance) / pi * 180);
+	}
+	else if (off_x == 0 && off_y < 0){
+	    return -90;
+	}
+	else if (off_x == 0 && off_y > 0){
+	    return 90;
+	}
+	else if (off_y == 0 && off_x < 0) {
+        return -180;
+    }
+	else {
+        if (off_y == 0 && off_x > 0) {
+            return 0;
+        };
+    }
+    return 0;
+
 }
 
-float get_distance(int off_x,int off_y)
+float get_distance(float off_x,float off_y)
 {
-	return sqrt(off_x*off_x + off_y*off_y);
+	return sqrtf(off_x*off_x + off_y*off_y);
 }
 
 
@@ -19,58 +44,57 @@ void rotate(float angle)
 {
 	//resetGyro(gyroSensor);
 
-	if(angle < 0)
-	{
-		printf("left %f  \n",angle);
-	}
-
-	if(angle > 0)
-	{
-	    printf("right %f \n",angle);
-	}
+//	if(angle > 0)
+//	{
+//		printf("left %f  \n",angle);
+//	}
+//
+//	if(angle < 0)
+//	{
+//	    printf("right %f \n",angle);
+//	}
 
 }
 
 void move(float distance)
 {
-	printf("робот проехал %f  робот повернул в ",distance);
+	printf("distance =  %f  \n",distance);
 }
 
 float move_to_next_coord(int c_x,int c_y,int n_x,int n_y,float r_angle)
 {
 	// get offset of current coord and next
-	int off_x = n_x - c_x;
-	int off_y = n_y - c_y;
+	float off_x = (float)(n_x - c_x);
+	float off_y = (float)(n_y - c_y);
 
 	float distance = get_distance(off_x,off_y);
 
 	// turn the robot in the direction to next coord
-	float angle = get_angle(off_x,off_y,distance);
-	angle = (angle <0) ? angle + r_angle : angle - r_angle;
+	float  angle = (float)get_angle(off_x,off_y,distance);
+	// change angle of robot
+	r_angle = angle - r_angle;
+	rotate(r_angle);
 
-	rotate(-angle);// vex angle are not correct
 
-	r_angle = angle; // change angle of robot
 
 	move(distance);	// move the robot to next coord
-	return r_angle;
+	return angle;
 }
 
 
 int main()
 {
     // angle between start pos and current
-	float r_angle = 0;
-
+	float r_angle = 90;
+	printf("a = %f\n",r_angle);
 	r_angle = move_to_next_coord(0,0,0,5,r_angle);
-	//printf("angle 1  %f \n",r_angle);
-	r_angle = move_to_next_coord(0,20,10,20,r_angle);
-	//printf("angle 2  %f \n",r_angle);
-	r_angle = move_to_next_coord(10,20,10,40,r_angle);
-	//printf("angle 3  %f \n",r_angle);
-
-	
-	r_angle = move_to_next_coord(10,40,10,10,r_angle);
-	//printf("angle 4  %f \n",r_angle);
+    printf("a = %f\n",r_angle);
+	r_angle = move_to_next_coord(0,5,0,20,r_angle);
+	printf("a = %f\n",r_angle);
+	r_angle = move_to_next_coord(0,20,20,40,r_angle);
+	printf("a = %f\n",r_angle);
+	r_angle = move_to_next_coord(20,40,10,10,r_angle);
+	printf("a = %f\n",r_angle);
+    printf("\nangle  = %f",r_angle);
 	return 0;
 }
